976081f831b4515579e775ed33c969431de14ab9
Avrohom Perl <info@avrohomperl.com>
2021-08-29 05:25:57 -0400

    Make postgresql work

--- app/databases.py
+++ app/databases.py
@@ -8,5 +8,10 @@ database_url = settings.database_url
 if database_url.split(":")[0] == "postgres":
     database_url = f"postgresql:{database_url.split(':')[1]}"
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(settings.database_url, echo=True, connect_args=connect_args)
+# check_same_thread is a sqlite specific setting
+if database_url.split(":")[0] == "sqlite":
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+
+engine = create_engine(database_url, echo=True, connect_args=connect_args)
