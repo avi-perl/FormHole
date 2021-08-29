b0a20eb1de6f6d6684196e040536165f027c47f2
Avrohom Perl <info@avrohomperl.com>
2021-08-29 05:18:34 -0400

    Make Heroku postgres URI work with SQL Alchemy

--- app/databases.py
+++ app/databases.py
@@ -2,5 +2,11 @@ from sqlmodel import create_engine
 
 from .config import settings
 
+# Heroku by default will use the URI of "postgres" when using their postgres sql instances.
+# This is not supported by SQL Alchemy. Updating it here makes deployment easy for users.
+database_url = settings.database_url
+if database_url.split(":")[0] == "postgres":
+    database_url = f"postgresql:{database_url.split(':')[1]}"
+
 connect_args = {"check_same_thread": False}
 engine = create_engine(settings.database_url, echo=True, connect_args=connect_args)
