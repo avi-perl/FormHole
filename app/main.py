ba0bd0b58850476ef416b43772ef899144c692ea
Avrohom Perl <info@avrohomperl.com>
2021-09-12 03:57:29 -0400

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .databases import create_db_and_tables
 from .config import settings
-from .routers import items, models
+from .routers import items, models, forms
 
 app = FastAPI(
     debug=settings.debug,
@@ -19,6 +20,10 @@ app = FastAPI(
             "name": "Models",
             "description": "Perform actions on specific model types.",
         },
+        {
+            "name": "Forms",
+            "description": "Create Items by submitting a form.",
+        },
     ],
     docs_url=settings.docs_url,
     redoc_url=settings.redoc_url,
@@ -32,6 +37,22 @@ app.include_router(
     models.router,
     prefix="/model",
     tags=["Models"],
+)
+app.include_router(
+    forms.router,
+    prefix="/form",
+    tags=["Forms"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_allow_origins,
+    allow_origin_regex=settings.cors_allow_origin_regex,
+    allow_methods=settings.cors_allow_methods,
+    allow_headers=settings.cors_allow_headers,
+    allow_credentials=settings.cors_allow_credentials,
+    expose_headers=settings.cors_expose_headers,
+    max_age=settings.cors_max_age,
 )
 
 
